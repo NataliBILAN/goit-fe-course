8020d093784bc21e7708ef38329af2375a9bd019
Nataliia Bilan <natali_bilan@ukr.net>
2018-06-18 11:39:28 +0300

    added test portfolio

---
+++ Portfolio/js/index.js
@@ -0,0 +1,12 @@
+let guest;
+let name = 'Mango';
+guest = 'Mango';
+console.log(guest);
+
+const day = 02;
+const month = 10;
+const year = 2017;
+
+const date = `${day}\\${month}\\${year}`;
+
+console.log(date);
