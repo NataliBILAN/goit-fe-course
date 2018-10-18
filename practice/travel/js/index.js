802457b4ef199c5ea0b7c8be6755fc861c64e893
Nataliia Bilan <natali_bilan@ukr.net>
2018-10-18 08:45:34 +0300

    added travel

---
+++ practice/travel/js/index.js
@@ -0,0 +1,17 @@
+'use strict'
+const buttonOpen = document.querySelector('.js-open-modal');
+const modalWindow = document.querySelector('.modal');
+const buttonClose = document.querySelector('.js-close-modal');
+
+function handleOpenWindow(event){
+    modalWindow.classList.add('.js-modal-show');
+}
+buttonOpen.addEventListener('click', handleOpenWindow);
+debugger
+function handleCloseWindow(event){
+   if(event.target.tagName === 'A'){
+        modalWindow.classList.remove('.js-modal-show');
+        modalWindow.classList.remove('.modal');
+   }
+}
+buttonClose.addEventListener('click', handleCloseWindow);
