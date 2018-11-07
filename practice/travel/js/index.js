d0d3a9195aeb9534044e8466487c5c796507e779
Nataliia Bilan <natali_bilan@ukr.net>
2018-11-07 19:13:36 +0200

    added correction

--- practice/travel/js/index.js
+++ practice/travel/js/index.js
@@ -1,12 +1,18 @@
+'use strict'
 $(function(){
     var $page = $('html, body');
-    $('.main-menu a[href*="#"]').click(function(){ 
+    $('.main-menu a[href*="#"], .to-page-nav').click(function(){ 
         $page.animate({
         scrollTop: $($.attr(this, 'href')).offset().top - 99
-    }, 700);
+    }, 900);
     return false;
 });
-
-})
-
-
+    $('#nav-toggle, .main-menu li a').click(function(){
+     $('.main-menu').toggleClass('menu-open');
+    });  
+    $(document).click(function(event) {
+        if ($(event.target).closest("#nav-toggle").length ) return;
+        $('.main-menu').removeClass('menu-open');
+        event.stopPropagation();
+    });
+})(jQuery);
